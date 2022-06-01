0007a1bd34c6ac7131f0152020f7ca7e31ddb7ef
deardaddy <sergpomorsky@gmail.com>
2022-06-01 22:39:20 +0300

    add first task

---
+++ Task_1_find_day_of_week/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Set number day");
+string day = Console.ReadLine();
+
+if (day == "1")
+{
+Console.WriteLine("Monday");
+}
+if (day == "2")
+{
+Console.WriteLine("Tuesday");
+}
+if (day == "3")
+{
+Console.WriteLine("Wednesday");
+}
+if (day == "4")
+{
+Console.WriteLine("Thursday");
+}
+if (day == "5")
+{
+Console.WriteLine("Friday");
+}
+if (day == "6")
+{
+Console.WriteLine("Saturday");
+}
+if (day == "7")
+{
+Console.WriteLine("Sunday");
+}
+if (day == "8")
+{
+Console.WriteLine("Error");
+}
+if (day == "9")
+{
+Console.WriteLine("Error");
+}
+if (day == "0")
+{
+Console.WriteLine("Error");
+}
