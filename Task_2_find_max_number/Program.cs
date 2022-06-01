50da698ef2e3bca33326e737ea6c6b31c2eaba32
deardaddy <sergpomorsky@gmail.com>
2022-06-01 22:42:42 +0300

    add second task

---
+++ Task_2_find_max_number/Program.cs
@@ -0,0 +1,10 @@
+﻿//find max number
+int a = 2;
+int b = 4;
+int c = 5;
+int max = 0;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.Write("max = ");
+Console.WriteLine(max);
